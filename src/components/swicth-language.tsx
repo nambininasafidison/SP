c41dc7b9fcd1b362c4155323b8349ec2a2d2464b
nambininasafidison <safidison23@gmail.com>
2025-01-10 12:36:42 +0300

    Split

--- src/components/swicth-language.tsx
+++ src/components/swicth-language.tsx
@@ -64,7 +64,9 @@ const languages = [
 export function SwitchLanguage(props: { className?: string }) {
   const { t, i18n } = useTranslation();
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("en");
+  const [value, setValue] = React.useState(
+    i18n.language.split("-")[0]! || "en"
+  );
 
   const handleLanguageChange = (lang: string) => {
     i18n.changeLanguage(lang);
